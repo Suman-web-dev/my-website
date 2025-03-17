33ed1ee97ac9a094f7c04a512260064e5666548b
Suman <sumanittechnologies@gmail.com>
2025-03-17 20:45:49 +0530

    update frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { CartProvider } from '../src/components/pages/CartContext.jsx'; // Make sure to import CartProvider
+import { CartProvider } from './components/pages/CartContext.jsx'; // Make sure to import CartProvider
 import { AuthProvider } from "./components/pages/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
