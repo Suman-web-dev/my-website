b132132c66174210a70e29c597622ceac15006cc
Suman <sumanittechnologies@gmail.com>
2025-03-20 23:45:23 +0530

    Build for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/my-react-project/', 
+  build: {
+    outDir: '../dist', // Output to the root dist folder
+  },
 })
