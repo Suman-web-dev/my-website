5903b4e4a0403bfb94d596c73e6f3179e5375532
Suman <sumanittechnologies@gmail.com>
2025-04-03 22:04:32 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
             <div className="col-lg-12">
                 {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
                 <div className="footer__copyright__text">
-                    <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i className="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                    <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This  is made with <i className="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Suman</a></p>
                 </div>
                 {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
             </div>
